7e3bb426300f434a5a0dcab7d6b27da0d32d579d
ChahanaSingh <147185022+ChahanaSingh@users.noreply.github.com>
2024-04-18 16:46:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var taskText = input.value;
+    if (taskText === "") {
+      alert("Please enter a task.");
+      return;
+    }
+    var ul = document.getElementById("taskList");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(taskText));
+    ul.appendChild(li);
+    input.value = "";
+  }
+  
